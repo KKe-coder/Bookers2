0d2cadf4ae8ba4d9ad50c8af3679c6e46e09739a
KKe-coder <carumag726@gmail.com>
2020-11-08 08:37:57 +0000

    [Update]books.edit.htmlの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
   end
 
