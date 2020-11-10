13f999bd1e97f9ee26f07d0ddf9ef35c08e68cc7
KKe-coder <carumag726@gmail.com>
2020-11-10 03:31:40 +0000

    [Update]S3 Font Awesomeアイコン以外全実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
       if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice:"You have created book successfully."
       else
        @books = Book.all
        render ("books/index")
@@ -28,12 +28,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id == current_user.id
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice:"You have updated book successfully."
     else
       render :edit
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user.id == current_user.id
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice:"You have updated user successfully."
     else
       render :edit
     end
